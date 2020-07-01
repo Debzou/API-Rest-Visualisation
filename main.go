c6e8159664bf3da7092718f3e86ee7dc9f92d7ff
Debzou <david.petit@etu.univ-smb.fr>
2020-07-01 13:58:03 +0200

    internal

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 import( 
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"github.com/Debzou/REST-API-GO/controllers"
+	controllers "github.com/Debzou/REST-API-GO/internal/controllers"
 )
 func main(){
-	router := gin.Default()
-	// This handler will match /user/john but will not match /user/ or /user
-	router.GET("/", controllers.wellcome())
-	router.Run(":8080")
+	// router := gin.Default()
+	// // This handler will match /user/john but will not match /user/ or /user
+	// router.GET("/", controllers.wellcome())
+	// router.Run(":8080")
+	controllers.wellcome()
 }
