54dd22ed5494dbf4b6bae35fb48e44d26a039aec
Debzou <david.petit@etu.univ-smb.fr>
2020-07-02 15:36:58 +0200

    test import models

--- internal/controllers/user.go
+++ internal/controllers/user.go
@@ -6,8 +6,17 @@ import(
 	"github.com/Debzou/REST-API-GO/internal/models"
 )
 
+
+type User struct {
+	ID       primitive.ObjectID `json:"_id,omitempty"`
+	Username string             `json:"username,omitempty"`
+	Password string             `json:"password,omitempty"`
+	Status   string             `json:"status,omitempty"`
+}
+
+
 func CreateUser(c *gin.Context) {
-	var user models.User
+	var user User
     c.BindJSON(user)
 	c.JSON(http.StatusOK, gin.H{"User": user.username})
 }
