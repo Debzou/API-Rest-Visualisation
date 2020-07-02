efda94ef69833ba3eb488fe663aac035be7f605e
Debzou <david.petit@etu.univ-smb.fr>
2020-07-02 16:25:34 +0200

    pretty

--- internal/controllers/user.go
+++ internal/controllers/user.go
@@ -10,10 +10,8 @@ import(
 
 func CreateUser(c *gin.Context) {
 	// create with models an user
-	todo := models.User{
-		Username: c.PostForm("username"),
-		Password: c.PostForm("password"),
-		Status: c.PostForm("status")
-	}
+	todo := models.User{Username: c.PostForm("username"),
+	Password: c.PostForm("password"),
+	Status: c.PostForm("status")}
 	c.JSON(http.StatusOK, gin.H{"User": todo.Username})
 }
