09346fcc192f1cf382dd9d7d7215f4a67a4fb953
Debzou <david.petit@etu.univ-smb.fr>
2020-07-02 13:05:18 +0200

    controller never used

--- internal/controllers/welcome.go
+++ internal/controllers/welcome.go
@@ -10,7 +10,6 @@ var IdentityKey = "id"
 // After auth, display a message
 func HelloHandler(c *gin.Context) {
 	claims := jwt.ExtractClaims(c)
-	user, _ := c.Get(IdentityKey)
 	c.JSON(200, gin.H{
 		"userID":   claims[IdentityKey],
 		"text":     "Hello World.",
