a3a74409649d72e5581e8d625f5aa5cba5a2d0c0
Debzou <david.petit@etu.univ-smb.fr>
2020-07-02 15:37:59 +0200

    test models

--- internal/controllers/user.go
+++ internal/controllers/user.go
@@ -7,16 +7,9 @@ import(
 )
 
 
-type User struct {
-	ID       primitive.ObjectID `json:"_id,omitempty"`
-	Username string             `json:"username,omitempty"`
-	Password string             `json:"password,omitempty"`
-	Status   string             `json:"status,omitempty"`
-}
-
 
 func CreateUser(c *gin.Context) {
 	var user User
     c.BindJSON(user)
-	c.JSON(http.StatusOK, gin.H{"User": user.username})
+	c.JSON(http.StatusOK, gin.H{"User": user.Username})
 }
