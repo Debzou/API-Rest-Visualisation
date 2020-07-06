457338c22f9614943d1d685624c73a2043253829
Debzou <david.petit@etu.univ-smb.fr>
2020-07-06 11:08:51 +0200

    creating new controllers for authenticate (user)

--- internal/controllers/user.go
+++ internal/controllers/user.go
@@ -1,5 +1,7 @@
+// define package name
 package controllers
 
+// import library
 import(
 	"time"
 	"context"
@@ -7,12 +9,15 @@ import(
 	"github.com/gin-gonic/gin"
 	"github.com/Debzou/REST-API-GO/internal/models"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/bson"
+	"log"
 )
 
 
 // DATABASE INSTANCE
 var collection *mongo.Collection
 
+// define the collection
 func UserCollection(c *mongo.Database) {
 	collection = c.Collection("users")
 }
@@ -21,9 +26,42 @@ func CreateUser(c *gin.Context) {
 	// create with models an user
 	user := models.User{Username: c.PostForm("username"),
 	Password: c.PostForm("password"),
-	Status: c.PostForm("status")}
-	// post data in mongodb
+	Status: "normal_user"}
+	// post user in mongodb
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	collection.InsertOne(ctx, user)
-	c.JSON(http.StatusOK, gin.H{"user status": user.Username})
+	// display message & httpstatus
+	c.JSON(http.StatusOK, gin.H{"message": "A user was been created with status : " + user.Status})
+	return
+}
+
+func AuthUser(username string,password string){
+	// init user structure
+	user := models.User{}
+	// define the context
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	// find the user with username 
+	err := collection.FindOne(ctx, bson.M{"username": username}).Decode(&user)
+	if err != nil {
+		log.Printf("Error, Reason: %v\n", err)
+		return
+	}
+	// check if password is good
+	return
+}
+
+func isExist(username string) bool{
+	// init user structure
+	user := models.User{}
+	// define the context
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	// find an user 
+	err := collection.FindOne(ctx,bson.M{"username": username}).Decode(&user)
+	// the user exist
+	if err != nil {
+		return true
+	// the user not exist
+	}else{
+		return false
+	}
 }
