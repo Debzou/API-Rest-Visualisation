da401553d93a689f9b05a895d5f13b3352068866
Debzou <david.petit@etu.univ-smb.fr>
2020-07-07 11:25:51 +0200

    launch-api become the main

--- launch-api.sh
+++ launch-api.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 docker rm $(docker ps -a -q)
+chmod +x ./packaged-api.sh
+./packaged-api.sh
 # sudo docker system prune -a
 docker build -t api1.0 .
-echo "output api :"
 # expose port 8080
 docker-compose up
